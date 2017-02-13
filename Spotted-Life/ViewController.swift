7f54891d80c904589fc6cf605135ac1cafbbb2a4
Simon Davies <simondmd@MacBook-Pro-4.local>
2017-02-13 11:29:09 +0100

    Initial Commit

---
+++ Spotted-Life/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spotted-Life
+//
+//  Created by Simon Davies on 13/02/2017.
+//  Copyright © 2017 Simon Davies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
